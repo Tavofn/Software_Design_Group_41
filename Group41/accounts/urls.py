0bbc463e0c44fe289feca00d1f2f765b32b6d5a7
gautamashutoshgautam <gautamashutosh.gautam@gmail.com>
2024-02-21 18:06:59 -0600

    Updated

--- Group41/accounts/urls.py
+++ Group41/accounts/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.logInPage, name='login'),
+    path('login/', views.logInPage, name='login'),
     path('fuel_quote_history/', views.fuelQuoteHistory),
     path('fuel_quote/', views.fuelQuote),
-    path('profile_management', views.profileManagement),
-    path('registration', views.registrationPage),
-    path('signup', views.signUp, name='signup'),
+    path('profile_management/', views.profileManagement),
+    path('registration/', views.registrationPage),
+    path('signup/', views.signUp, name='signup'),
 
 ]
