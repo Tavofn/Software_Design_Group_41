446a736f773742fd039728ccee79897dc5db0b5a
Henry Palma <tavovnt@gmail.com>
2024-03-21 17:30:06 -0500

    in process of login validation

--- Group41/accounts/urls.py
+++ Group41/accounts/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.logInPage, name='login'),
     path('', views.logInPage, name='login'),
     path('fuel_quote_history/', views.fuelQuoteHistory, name='fuel_quote_history'),
     path('fuel_quote/', views.fuelQuote, name='fuel_quote'),
--- Group41/accounts/views.py
+++ Group41/accounts/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 from django.http import HttpResponse
 # Create your views here.
 
 def logInPage(request):
-    return render (request, 'accounts/login.html')
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("fuel_quote")
+        else:
+            print("not working")
+            messages.success(request,("There was an error loggin in, try again."))
+            return redirect("/")        
+            
+    else:
+        return render (request, 'accounts/login.html')
+
 def fuelQuoteHistory(request):
     return render (request, 'accounts/fuel_quote_history.html')
 def fuelQuote(request):
