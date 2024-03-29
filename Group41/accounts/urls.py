87a352a6c16bad2ae43eca6732ccb135aa0fc291
Ashutosh Gautam <gautamashutosh.gautam@gmail.com>
2024-03-28 20:38:44 -0500

    Updated

--- Group41/accounts/urls.py
+++ Group41/accounts/urls.py
@@ -1,10 +1,42 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views # for reset password
 
 urlpatterns = [
-    path('', views.logInPage, name='login'),
-    path('fuel_quote_history/', views.fuelQuoteHistory, name='fuel_quote_history'),
-    path('fuel_quote/', views.fuelQuote, name='fuel_quote'),
-    path('profile_management/', views.profileManagement),
-    path('registration/', views.registrationPage, name='registration'),
-] 
+    path('', views.home, name='home'),
+    path('products/', views.products, name='products'),
+    path('customer/<str:pk>/', views.customer, name='customer'),
+    path('create_order/<str:pk>/', views.createOrder, name='createOrder'),
+    path('update_order/<str:pk>/', views.updateOrder, name='updateOrder'),
+    path('delete_order/<str:pk>/', views.deleteOrder, name='delete_order'),
+    path('all-orders/', views.allOrders, name='allOrders'),
+    path('register/', views.registerPage, name='register'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('user/', views.userPage, name='user-page'),
+    path('account/', views.accountSettings, name='account'),
+
+    path('reset_password/',
+     auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"),
+     name="reset_password"),
+
+    path('reset_password_sent/',
+        auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"),
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"),
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/',
+        auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"),
+        name="password_reset_complete"),
+]
+
+
+'''
+1 - Submit email form                         //PasswordResetView.as_view()
+2 - Email sent success message                //PasswordResetDoneView.as_view()
+3 - Link to password Rest form in email       //PasswordResetConfirmView.as_view()
+4 - Password successfully changed message     //PasswordResetCompleteView.as_view()
+'''
--- Group41/accounts/views.py
+++ Group41/accounts/views.py
@@ -1,34 +1,194 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate,login,logout
-from django.contrib import messages
 from django.http import HttpResponse
-# Create your views here.
+from .models import *
+from .forms import OrderForm, CreateUserForm, CustomerForm
+from django.forms import inlineformset_factory ## multiple forms in forms
+from .filters import OrderFilter
+from django.contrib.auth.forms import UserCreationForm # django default form
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required # restricted user access
+from .decorators import unauthenticated_user, allowed_users, admin_only # custom restricted and allowed user acces
+from django.contrib.auth.models import Group # Django signal to associated user with group
 
-def logInPage(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
+# Create your views here.
+@login_required(login_url='login')
+@admin_only # if customer the redirect to user-page and if admin redirect to view_func
+def home(request):
+    last_five_orders = Order.objects.all().order_by('-date_created')[:5]
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+    total_orders = orders.count()
+
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {
+        'last_five_orders':last_five_orders,
+        'orders':orders,
+        'customers':customers,
+        'total_customers':total_customers,
+        'total_orders':total_orders,
+        'delivered':delivered,
+        'pending':pending
+    }
+
+    return render(request, 'accounts/dashboard.html', context)
+
+@login_required(login_url='login')
+@admin_only # if customer the redirect to user-page and if admin redirect to view_func
+def allOrders(request):
+    orders = Order.objects.all()
+    context = {
+        'orders':orders,
+    }
+
+    return render(request, 'accounts/all_orders.html', context)
+
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin']) # we can add more like - ['admin', 'staff', etc]
+def products(request):
+    products = Product.objects.all()
+
+    return render(request, 'accounts/products.html', {'products':products})
+
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin']) # we can add more like - ['admin', 'staff', etc]
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+
+    orders = customer.order_set.all()  # order_set means customers child order from model fields
+    order_count = orders.count()
+
+    myFilter = OrderFilter(request.GET, queryset=orders)
+    orders = myFilter.qs
+
+    context = {
+        'customer':customer,
+        'orders':orders,
+        'order_count':order_count,
+        'myFilter':myFilter
+    }
+
+    return render(request, 'accounts/customer.html', context)
+
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin']) # we can add more like - ['admin', 'staff', etc]
+def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=3) # Parent model and Child model. extra=3 means you will see three times form to place order 3 items together. You can use extra=10 or extra=5 as you wish.
+    customer = Customer.objects.get(id=pk)
+
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer)
+
+    # form = OrderForm(initial={'customer':customer}) # To see the customer in form, for which customer profile i viewd.
+
+    if request.method == 'POST':
+        # form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
+            return redirect('home')
+
+    context = {'formset':formset}
+    return render(request, 'accounts/order_form.html', context)
+
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin']) # we can add more like - ['admin', 'staff', etc]
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+
+    form = OrderForm(instance=order) # to get pre-field form
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order) # Update (instance) and Post
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form':form}
+    return render(request, 'accounts/update_order_form.html', context)
+
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin']) # we can add more like - ['admin', 'staff', etc]
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+
+    if request.method == 'POST':
+        order.delete()
+        return redirect('home')
+
+    context = {'item':order}
+    return render(request, 'accounts/delete.html', context)
+
+@unauthenticated_user
+def registerPage(request):
+    form = CreateUserForm()  # Now UserCreationForm will replace by CreateUserForm
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)  # Now UserCreationForm will replace by CreateUserForm
+        if form.is_valid():
+            user = form.save() # to associated user with group
+            username = form.cleaned_data.get('username') # to associated user with group
+            messages.success(request, 'Account was create for ' + username) # to associated user with group
+            return redirect('login')
+
+    context = {'form':form}
+    return render(request, 'accounts/register.html', context)
+
+@unauthenticated_user
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password) # to check the user is in model/db or not.
         if user is not None:
             login(request, user)
-            return redirect("fuel_quote")
+            return redirect('home')
         else:
-            print("not working")
-            messages.success(request,("There was an error loggin in, try again."))
-            return redirect("/")        
-            
-    else:
-        return render (request, 'accounts/login.html')
+            messages.info(request, 'Username or Password is Incorrect')
 
-def fuelQuoteHistory(request):
-    return render (request, 'accounts/fuel_quote_history.html')
-def fuelQuote(request):
-    return render (request, 'accounts/fuel_quote.html')
-def profileManagement(request):
-    return render (request, 'accounts/profile_management.html')
-def registrationPage(request):
-    return render (request, 'accounts/registration.html')
-def signUp(request):
-    return render (request, 'accounts/signup.html')
+    context = {}
+    return render(request, 'accounts/login.html', context)
 
- 
+
+def logoutUser(request):
+    logout(request)
+
+    return redirect('login')
+
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
+def userPage(request):
+    orders = request.user.customer.order_set.all()
+
+    total_orders = orders.count()
+
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders':orders, 'total_orders':total_orders, 'delivered':delivered, 'pending':pending}
+    return render(request, 'accounts/user.html', context)
+
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
+def accountSettings(request):
+    customer = request.user.customer # logged in user
+    form = CustomerForm(instance=customer)
+
+    if request.method == 'POST':
+        form = CustomerForm(request.POST, request.FILES, instance=customer)
+        if form.is_valid():
+            form.save()
+
+    context = {'form':form}
+    return render(request, 'accounts/account_settings.html', context)
