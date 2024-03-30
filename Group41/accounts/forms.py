a52e8cd2d0fa6afa35ee7c77fb3c79d8988a6744
rohitadhikarii <rohitadhikarii28@gmail.com>
2024-03-29 20:41:36 -0500

    made some changes

--- Group41/accounts/forms.py
+++ Group41/accounts/forms.py
@@ -2,12 +2,12 @@ from django.forms import ModelForm
 from .models import *
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from django.contrib.auth.models import User   # django default user model
+from django.contrib.auth.models import User   # using the django default user model
 
 class OrderForm(ModelForm): # name of the model and Form. and inherit from ModelForm.
     class Meta:
         model = Order # which model
-        fields = '__all__' # which fields i will allow - ['customer', 'product']. but i take here all.
+        fields = '__all__' # which fields are allowed - ['customer', 'product']. 
 
 
 class CreateUserForm(UserCreationForm):
